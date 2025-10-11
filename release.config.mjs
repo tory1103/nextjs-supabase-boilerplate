efa365bb998f34091f2fd25b875abf0493a3e8de
adriantoral <adrian.rubiboy11@gmail.com>
2025-10-11 10:59:15 +0200

    feat: add project configuration files and auth action exports

---
+++ release.config.mjs
@@ -0,0 +1,41 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
+const releaseConfig = {
+	branches: ['main'],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				releaseRules: [
+					{ type: 'feat', release: 'minor' },
+					{ type: 'fix', release: 'patch' },
+					{ type: 'chore', release: false },
+				],
+				parserOpts: {
+					mergeCommitPattern: /^Merge pull request #(\d+) from (.*)$/,
+					mergeCorrespondence: ['id'],
+				},
+			},
+		],
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'CHANGELOG.md',
+			},
+		],
+		[
+			'@semantic-release/npm',
+			{
+				npmPublish: false,
+			},
+		],
+		'@semantic-release/git',
+		'@semantic-release/github',
+	],
+};
+
+export default releaseConfig;
---
+++ src/actions/index.ts
@@ -0,0 +1,4 @@
+export * from './auth/types';
+export * from './auth/signup';
+export * from './auth/signin';
+export * from './auth/signout';
